9959b2017f5f4f99e55db137f724741a68e84e84
AceIsHuman <menoman1995@yahoo.com>
2019-11-13 21:37:42 -0600

    create get all acounts endpoint

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const server = require('./server.js');
+const db = require('./data/dbConfig');
 
 const PORT = process.env.PORT || 4000;
+
+// GET ALL ACCOUNTS
+server.get('/api/accounts', async (req, res) => {
+  try {
+    const accounts = await db('*').from('accounts');
+    accounts.length ?
+      res.status(200).json({accounts}) :
+      res.status(200).json({ message: 'There are no accounts' });
+  } catch(err) {
+    res.status(500).json({ errorMessage: 'Internal Server Error' });
+  }
+});
 
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
